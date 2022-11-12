994b65c7d31b90d3467435398063689cbb6f0c9a
Veeki Yadav <veekiyadav1819@gmail.com>
2022-11-12 16:57:10 +0530

    [feat-002]: Serve static files

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const loginRoute = require('./routes/loginRoutes');
 const middlewares = require('./middlewares');
+const path = require('path');
 
 const app = express();
 const port = 3000;
@@ -8,6 +9,9 @@ const server = app.listen(port, () => {console.log('Listening on port 3000')});
 
 app.set("view engine", "pug");
 app.set("views", "views");
+
+// serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/login', loginRoute);
