ffe5458603322ab7fa234fb4469bbf418a7f01d5
Veeki Yadav <veekiyadav1819@gmail.com>
2022-11-14 10:15:49 +0530

    [feat-005]: Add login/logout

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
 const express = require('express');
 const loginRoute = require('./routes/loginRoutes');
 const registerRoute = require('./routes/registerRoutes');
+const logoutRoute = require('./routes/logoutRoutes');
 const middlewares = require('./middlewares');
 const path = require('path');
 const bodyParser = require('body-parser');
 const database = require('./database');
+const session = require('express-session');
 
 const app = express();
 const port = 3000;
-const server = app.listen(port, () => {console.log('Listening on port 3000')});
+const server = app.listen(port, () => {
+	console.log('Listening on port 3000');
+});
 
-app.set("view engine", "pug");
-app.set("views", "views");
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public'))); // serve static files
+app.use(
+	session({
+		secret: 'twitter clone',
+		resave: true,
+		saveUninitialized: false,
+	})
+);
 
 // Routes
 app.use('/login', loginRoute);
 app.use('/register', registerRoute);
+app.use('/logout', logoutRoute);
 
-app.get("/", middlewares.requireLogin, (req, res, next)=> {
-    res.status(200).render("home");
-})
+app.get('/', middlewares.requireLogin, (req, res, next) => {
+	var payload = {
+		pageTitle: 'Home',
+		userLoggedIn: req.session.user,
+	};
+
+	res.status(200).render('home', payload);
+});
